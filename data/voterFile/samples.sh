feeb9f067995478a1260025c277a03dcac65c66e
KirkHadley <fhadley0411@gmail.com>
2017-03-02 04:28:02 +0000

    rest of the sample files

--- data/voterFile/samples.sh
+++ data/voterFile/samples.sh
@@ -1,21 +1,33 @@
 #! bin/bash
 
-find . -name "*.csv"|while read fname; do
-  echo $fname
-  head -n 100 $fname > $fname.sample 
-done
-find . -name "*.CSV"|while read fname; do
-  echo $fname
-  head -n 100 $fname > $fname.sample 
-done
-
-find . -name "*.TXT"|while read fname; do
-  echo $fname
-  head -n 100 $fname > $fname.sample 
-done
+#find . -name "*.csv"|while read fname; do
+#  echo $fname
+#  head -n 100 $fname > $fname.sample 
+#done
+#find . -name "*.CSV"|while read fname; do
+#  echo $fname
+#  head -n 100 $fname > $fname.sample 
+#done
+#
+#find . -name "*.TXT"|while read fname; do
+#  echo $fname
+#  head -n 100 $fname > $fname.sample 
+#done
 #find . -name "*.txt"|while read fname; do
 #  echo $fname
 #  head -n 100 $fname > $fname.sample 
 #done
 #
 #
+find . -name "foi_bas*"|while read fname; do
+  echo $fname
+  head -n 100 $fname > $fname.sample 
+done
+find . -name "foi_his*"|while read fname; do
+  echo $fname
+  head -n 100 $fname > $fname.sample 
+done
+find . -name "entire_state*"|while read fname; do
+  echo $fname
+  head -n 100 $fname > $fname.sample 
+done
